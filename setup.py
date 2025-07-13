5dbdddd6ad952fdb6289b10e2276d0b9ccb8c59a
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2025-07-13 14:23:10 -0400

    Add [dev] extras to setup.py

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     install_requires=open('requirements.txt').readlines(),
     extras_require={
         'webserver': open('requirements.txt').readlines() + ['pywebio'],
+        'dev': open('requirements-dev.txt').readlines(),
     },
     classifiers=[
         "Programming Language :: Python :: 3",
